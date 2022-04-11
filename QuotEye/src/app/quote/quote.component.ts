dd0a489e5acb6b4fa97ff0187964d1b805706787
Benie <davinci.monalissa@gmail.com>
2022-04-11 09:38:18 +0300

    Add property values for the Quote Component Array's index, quote, author, user-name, date, downvotes, and upvotes.

--- QuotEye/src/app/quote/quote.component.ts
+++ QuotEye/src/app/quote/quote.component.ts
@@ -1,3 +1,4 @@
+import { Quote } from '../quote';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
-  quotes:Quotes[] = [
-    
-  ]
+  quotes:Quote[] = [
+    new Quote(1,'Quote 1','Author 1','User 1',new Date(2022,4,14),20,3),
+    new Quote(2,'Quote 2','Author 2','User 2',new Date(2022,1,15),20,3),
+    new Quote(3,'Quote 3','Author 3','User 3',new Date(2022,2,17),20,3),
+  ];
 
   constructor() { }
 
