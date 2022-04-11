ad912449277bd02d358ed7683ec3cf34d4a971db
Benie <davinci.monalissa@gmail.com>
2022-04-11 09:21:47 +0300

    Create a new object with an empty array of properties.

--- QuotEye/src/app/quote/quote.component.ts
+++ QuotEye/src/app/quote/quote.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes:Quotes[] = [
+    
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
