20653078ef128e4b09a4131232c49053d88d364e
Benie <davinci.monalissa@gmail.com>
2022-04-11 09:43:20 +0300

    Add a data type for the toggle and include it in the constructor function--all these at the Quote  Class.

--- QuotEye/src/app/quote.ts
+++ QuotEye/src/app/quote.ts
@@ -1,6 +1,9 @@
 export class Quote {
 
+    public showDescription: boolean;
+
     constructor(public id: number,public description: string,public author: string,public name: string,public completeDate: (Date),public upvotes: number,public downvotes: number){
 
+        this.showDescription = false;
     }
 }
