9e78d0bb1ac8083a146efa776feab19bb73266ad
Benie <davinci.monalissa@gmail.com>
2022-04-11 09:31:53 +0300

    Add a constructor functtion to the Quote Class, with property names for the Quote Component array.

--- QuotEye/src/app/quote.ts
+++ QuotEye/src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+
+    constructor(public id: number,public description: string,public author: string,public name: string,public completeDate: (Date),public upvotes: number,public downvotes: number){
+
+    }
 }
