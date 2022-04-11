7f74d803f4868b7427db012478c0f2dbd081f770
Benie <davinci.monalissa@gmail.com>
2022-04-11 11:59:58 +0300

    Create a pipe for use on counting the date.

--- QuotEye/src/app/app.module.ts
+++ QuotEye/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { SocialComponent } from './social/social.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SocialComponent } from './social/social.component';
     QuoteComponent,
     QuoteDetailComponent,
     QuoteFormComponent,
-    SocialComponent
+    SocialComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
