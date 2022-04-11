857bf3f884ef945c28acf5076853cee9483f1983
Benie <davinci.monalissa@gmail.com>
2022-04-11 11:37:28 +0300

    Create a new component--Social Component--to hold social icons and a toggle containing them.

--- QuotEye/src/app/app.module.ts
+++ QuotEye/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
