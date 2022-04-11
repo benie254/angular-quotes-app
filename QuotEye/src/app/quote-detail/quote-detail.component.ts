e77439e39318b5e900dbee7303a4dfb277140103
Benie <davinci.monalissa@gmail.com>
2022-04-11 10:21:34 +0300

    Add functions for upvote and downvote, and include a function for Quote delete to the Quote Details component.

--- QuotEye/src/app/quote-detail/quote-detail.component.ts
+++ QuotEye/src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-quote-detail',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+
+  @Input() quote: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
+  downvote = 0;
+  upvote = 0;
+
+  voteUp(){
+    this.upvote = this.upvote + 1;
+  }
+  voteDown(){
+    this.downvote = this.downvote + 1;
+  }
 
   constructor() { }
 
