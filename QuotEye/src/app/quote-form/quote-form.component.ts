06dfdd25579662d44d175040d5832858f9585582
Benie <davinci.monalissa@gmail.com>
2022-04-11 11:33:20 +0300

    Create a function for the form submit at the Quote Component's class.

--- QuotEye/src/app/quote-form/quote-form.component.ts
+++ QuotEye/src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+import { QuoteDetailComponent } from '../quote-detail/quote-detail.component';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quote(0,``,'','',new Date(),0,0);
+
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0,``,'','',new Date(),0,0)
+  }
 
   constructor() { }
 
--- QuotEye/src/app/quote/quote.component.ts
+++ QuotEye/src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1,'Quote 1','Author 1','User 1',new Date(2022,4,14),20,3),
-    new Quote(2,'Quote 2','Author 2','User 2',new Date(2022,1,15),20,3),
-    new Quote(3,'Quote 3','Author 3','User 3',new Date(2022,2,17),20,3),
+    new Quote(1,`Quote 1`,'Author 1','User 1',new Date(2022,4,14),20,3),
+    new Quote(2,`Quote 2`,'Author 2','User 2',new Date(2022,1,15),20,3),
+    new Quote(3,`Quote 3`,'Author 3','User 3',new Date(2022,2,17),20,3),
   ];
 
   toggleDetails(index){
